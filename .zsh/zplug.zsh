c618902d7ce1e7716e667a70be8fc042d3b57977
Kanji Takahashi <takahashi@jnlp.org>
2017-08-19 08:17:52 +0900

    plugの設定

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -1,7 +1,11 @@
+# 最初にzplugを読み込む
 zplug "zplug/zplug", hook-build:'zplug --self-manage'
 
+# completionのシェルスクリプトはz先に読み込む必要がある（sh設定ファイルで読み込むため。）
+zplug "~/.zsh/completion", from:local, use:"*.sh"
 zplug "~/.zsh", from:local, use:"<->_*.zsh"
 
+# zshのプラグイン
 zplug "zsh-users/zsh-syntax-highlighting"
 zplug "zsh-users/zsh-completions"
 zplug "zsh-users/zsh-history-substring-search"
