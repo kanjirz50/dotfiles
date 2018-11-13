d8cdda42d5abe20f7108ab6c1c32449f8e03d7dc
Kanji Takahashi <ka.takahashi@sansan.com>
2018-11-13 10:18:22 +0900

    pyenv virtualenvとthefuckの設定

--- .zsh/05_mac.zsh
+++ .zsh/05_mac.zsh
@@ -2,5 +2,8 @@
 export PYENV_ROOT=$HOME/.pyenv
 export PATH=$PYENV_ROOT/bin:$PATH
 eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 zplug $(pyenv root)/completions/pyenv.zsh, from:local
+
+eval "$(thefuck --alias)"
